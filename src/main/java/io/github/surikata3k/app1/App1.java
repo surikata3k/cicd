d932bba77148ce2cefafdfc46632f0ebc186e0a6
surikata3k@gmail.com <surikata3k@gmail.com>
2025-01-21 15:59:25 +0100

    First file from App1

---
+++ src/main/java/io/github/surikata3k/app1/App1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package io.github.surikata3k.app1;
+
+/**
+ *
+ * @author roger
+ */
+public class App1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
